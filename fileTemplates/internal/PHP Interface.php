94f5fd176d259e0a3a879398e09304f572e081f7
Valdeci Jr <valdeci.junior@izap.com.br>
2016-01-20 12:37:14 -0300

    File code templates

---
+++ fileTemplates/internal/PHP Interface.php
@@ -0,0 +1,17 @@
+<?php
+#parse("PHP File Header.php")
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+
+/**
+ * ${NAME}
+ * 
+ * @author ${USER}
+ * @version 1.0
+ */
+interface ${NAME} {
+
+}
